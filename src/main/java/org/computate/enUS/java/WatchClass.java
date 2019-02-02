04f287bda019a6ba15a9934551121f2d1ececd9f
ctate <computate@computate.org>
2019-02-01 23:05:43 -0700

    Updated watch code.

--- src/main/java/org/computate/enUS/java/WatchClass.java
+++ src/main/java/org/computate/enUS/java/WatchClass.java
@@ -1,10 +1,10 @@
 package org.computate.enUS.java;
 
 import java.io.File;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.solr.common.SolrInputDocument;
+import org.computate.enUS.java.WatchClass;
 
 public class WatchClass extends WriteAllClasses {
 
@@ -76,56 +76,4 @@ public class WatchClass extends WriteAllClasses {
 			}
 		}
 	}
-
-//	@Test()
-//	public void  testStuff() throws Exception, Exception {
-//		Integer entiteNumero = 0;
-//		String result = StringUtils.join(++entiteNumero > 0 ? ", " : "", "-stuff");
-//		result.toString();
-////		String appName = "computate";
-////		String appPath = "/usr/local/src/" + appName;
-////		String classAbsolutePath = "/usr/local/src/computate/src/main/java/org/computate/frFR/java/WatchClassBase.java";
-////		String classAbsolutePath = "/usr/local/src/computate/src/main/java/org/computate/frFR/couverture/Couverture.java";
-////		String classAbsolutePath = appPath + "/src/main/java/org/computate/frFR/java/RegarderRepertoire.java";
-////		String classAbsolutePath = "/usr/local/src/computate/src/main/java/org/computate/frFR/java/EcrireGenClasse.java";
-////		String classAbsolutePath = "/usr/local/src/computate/src/main/java/org/computate/frFR/java/EcrireToutesClasses.java";
-////		String classAbsolutePath = "/usr/local/src/computate/src/main/java/org/computate/frFR/java/EcrireClasse.java";
-////		String classAbsolutePath = "/usr/local/src/computate/src/main/java/org/computate/frFR/java/StringPrintWriter.java";
-////		String appName = "computate.org";
-////		String appPath = "/usr/local/src/computate.org";
-////		String classAbsolutePath = "/usr/local/src/computate.org/src/main/java/org/computate/frFR/site/cours/c000/config/ConfigSite.java";
-////		String classAbsolutePath = "/usr/local/src/computate.org/src/main/java/org/computate/frFR/site/cours/c000/contexte/SiteContexte.java";
-////		String appName = "computate-cardiac";
-////		String appPath = "/usr/local/src/" + appName;
-////		String classAbsolutePath = appPath + "/src/main/java/org/computate/frFR/cardiaque/requete/RequeteSite.java";
-////		String classAbsolutePath = appPath + "/src/main/java/org/computate/frFR/cardiaque/config/ConfigSite.java";
-////		String classAbsolutePath = appPath + "/src/main/java/org/computate/frFR/cardiaque/warfarin/CalculInr.java";
-////		String classAbsolutePath = appPath + "/src/main/java/org/computate/frFR/cardiaque/cluster/Cluster.java";
-////		String appName = "computate-scolaire";
-////		String appPath = "/usr/local/src/computate-scolaire";
-////		String classAbsolutePath = "/usr/local/src/computate-scolaire/src/main/java/org/computate/frFR/scolaire/client/org/ClientOrg.java";
-////		String classAbsolutePath = "/usr/local/src/computate-scolaire/src/main/java/org/computate/frFR/scolaire/ecole/EcoleScolaire.java";
-////		String appName = "vertx-art";
-////		String appPath = "/usr/local/src/vertx-art";
-////		String classAbsolutePath = "/usr/local/src/vertx-art/src/main/java/org/computate/frFR/vertx/art/moisson/MoissoneurOai.java";
-//		String appName = "citi-architect";
-//		String appPath = "/home/ctate/workspace-citi/" + appName;
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/architect/team/promotions/RepoCommon.java";
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/architect/repo/TeamRepo.java";
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/architect/team/promotions/TeamPromotions.java";
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/architect/product/automation/AutomationBase.java";
-//		String classAbsolutePath = appPath + "/src/main/java/com/citi/commonwealth/promotions/model/Adjustment.java";
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/commonwealth/payments/integration/rplid/service/AutpayI18n.java";
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/enUS/commonwealth/payments/integration/rplid/service/AutopayI18n.java";
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/commonwealth/promotions/model/AdjustmentReasonCodes.java";
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/commonwealth/promotions/model/EligibleAdjustmentType.java";
-////		String classAbsolutePath = appPath + "/src/main/java/com/citi/commonwealth/promotions/model/Adjustment.java";
-//		String[] args = ArrayUtils.toArray(appPath, classAbsolutePath);
-//		WatchClass watchClass = new WatchClass();
-//		watchClass.args = args;
-//		watchClass.appName = appName;
-//		watchClass.appPath = appPath;
-//		watchClass.initWatchClassBase(); 
-//		watchClass(watchClass);
-//	}
 }
