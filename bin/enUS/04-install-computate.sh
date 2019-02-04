ee6701ea743065779d4dc820c6d9bad154e0b6dc
ctate <computate@computate.org>
2019-02-04 06:58:28 -0700

    Removed the watch code for computate project.

--- bin/enUS/04-install-computate.sh
+++ bin/enUS/04-install-computate.sh
@@ -7,26 +7,3 @@ source "$appComputatePath/bin/enUS/00-functions.sh"
 #############
 
 computate "cd $appComputatePath && mvn install"
-
-computate "echo '
-[Unit]
-Description=Watch the computate app and generate Java code when the Java source files are modified. 
-After=network.target
-
-[Service]
-Type=simple
-User=$USER
-Group=$USER
-WorkingDirectory=$appPath
-Environment=appName=computate
-Environment=appPath=$appComputatePath
-Environment=appComputatePath=$appComputatePath
-ExecStart=$appComputatePath/bin/$languageName/watch.sh
-
-[Install]
-WantedBy=multi-user.target
-' | sudo tee /usr/lib/systemd/system/watch-computate.service"
-computate "sudo systemctl daemon-reload"
-computate "sudo systemctl restart watch-computate.service"
-computate "systemctl status watch-computate.service --no-pager"
-computate "sudo systemctl enable watch-computate.service"
--- bin/frFR/04-installer-computate.sh
+++ bin/frFR/04-installer-computate.sh
@@ -6,26 +6,3 @@ source "$(dirname $0)/00-functions.sh"
 #############
 
 computate "cd $appliChemin && mvn install"
-
-computate "echo '
-[Unit]
-Description=Regarder l'"'"'"'"'"'"'appli computate et générer decode Java lorsque les fichiers source Java sont modifiés. 
-After=network.target
-
-[Service]
-Type=simple
-User=$USER
-Group=$USER
-WorkingDirectory=$appliChemin
-Environment=appliNom=$appliNom
-Environment=appliChemin=$appliChemin
-Environment=appliComputateChemin=$appliComputateChemin
-ExecStart=$appliComputateChemin/bin/$langueNom/regarder.sh
-
-[Install]
-WantedBy=multi-user.target
-' | sudo tee /usr/lib/systemd/system/regarder-$appliNom.service"
-computate "sudo systemctl daemon-reload"
-computate "sudo systemctl restart regarder-$appliNom.service"
-computate "systemctl status regarder-$appliNom.service --no-pager"
-computate "sudo systemctl enable regarder-$appliNom.service"
