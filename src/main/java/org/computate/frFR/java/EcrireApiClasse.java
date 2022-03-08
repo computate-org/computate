78a26c586ce072e7b698c6e42509ec9e1eb8905c
Christopher Tate <computate@computate.org>
2022-03-07 21:45:38 -0700

    Fixing solr pivot search.

--- src/main/java/org/computate/frFR/java/EcrireApiClasse.java
+++ src/main/java/org/computate/frFR/java/EcrireApiClasse.java
@@ -3648,12 +3648,12 @@ public class EcrireApiClasse extends EcrireGenClasse {
 							tl(4, "}");
 							tl(3, "}");
 							l();
-							tl(3, "SolrResponse.FacetPivots facetPivots = Optional.ofNullable(", classeLangueConfig.getString(ConfigCles.var_reponse), classeLangueConfig.getString(ConfigCles.var_Recherche), ".getFacetCounts()).map(f -> f.getFacetPivots()).orElse(null);");
-							tl(3, "if(facetPivots != null) {");
+							tl(3, "SolrResponse.FacetPivot facetPivot = Optional.ofNullable(", classeLangueConfig.getString(ConfigCles.var_reponse), classeLangueConfig.getString(ConfigCles.var_Recherche), ".getFacetCounts()).map(f -> f.getFacetPivot()).orElse(null);");
+							tl(3, "if(facetPivot != null) {");
 							tl(4, "JsonObject facetPivotJson = new JsonObject();");
 							tl(4, "json.put(\"facet_pivot\", facetPivotJson);");
-							tl(4, "for(SolrResponse.FacetPivot facetPivot : facetPivots.getPivots().values()) {");
-							tl(5, "String[] vars", classeLangueConfig.getString(ConfigCles.var_Indexe), " = facetPivot.getName().trim().split(\",\");");
+							tl(4, "for(SolrResponse.Pivot pivot : facetPivot.getPivot().values()) {");
+							tl(5, "String[] vars", classeLangueConfig.getString(ConfigCles.var_Indexe), " = pivot.getName().trim().split(\",\");");
 							tl(5, "String[] ", classeLangueConfig.getString(ConfigCles.var_entite), "Vars = new String[vars", classeLangueConfig.getString(ConfigCles.var_Indexe), ".length];");
 							tl(5, "for(Integer i = 0; i < ", classeLangueConfig.getString(ConfigCles.var_entite), "Vars.length; i++) {");
 							tl(6, "String ", classeLangueConfig.getString(ConfigCles.var_entite), classeLangueConfig.getString(ConfigCles.var_Indexe), " = vars", classeLangueConfig.getString(ConfigCles.var_Indexe), "[i];");
@@ -3661,7 +3661,7 @@ public class EcrireApiClasse extends EcrireGenClasse {
 							tl(5, "}");
 							tl(5, "JsonArray pivotArray = new JsonArray();");
 							tl(5, "facetPivotJson.put(StringUtils.join(", classeLangueConfig.getString(ConfigCles.var_entite), "Vars, \",\"), pivotArray);");
-							tl(5, classeLangueConfig.getString(ConfigCles.var_reponse), "Pivot", classeApiMethode, classeNomSimple, "(facetPivot.getPivot(), pivotArray);");
+							tl(5, classeLangueConfig.getString(ConfigCles.var_reponse), "Pivot", classeApiMethode, classeNomSimple, "(pivot.getInternalPivot(), pivotArray);");
 							tl(4, "}");
 							tl(3, "}");
 							tl(3, "if(exception", classeLangueConfig.getString(ConfigCles.var_Recherche), " != null) {");
@@ -3732,7 +3732,7 @@ public class EcrireApiClasse extends EcrireGenClasse {
 						tl(3, "pivotJson.put(\"value\", pivotField.getValue());");
 						tl(3, "pivotJson.put(\"count\", pivotField.getCount());");
 						tl(3, "Collection<SolrResponse.PivotRange> pivotRanges = pivotField.getRanges().values();");
-						tl(3, "List<SolrResponse.Pivot> pivotFields2 = pivotField.getPivot();");
+						tl(3, "List<SolrResponse.Pivot> pivotFields2 = pivotField.getInternalPivot();");
 						tl(3, "if(pivotRanges != null) {");
 						tl(4, "JsonObject rangeJson = new JsonObject();");
 						tl(4, "pivotJson.put(\"ranges\", rangeJson);");
