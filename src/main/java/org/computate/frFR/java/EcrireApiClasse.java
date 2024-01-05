99f7ca885870ef555c0f304fb9ea9cb91d0d65c1
Christopher Tate <computate@computate.org>
2024-01-05 13:48:04 -0700

    Fixing a bug with JsonArrays in API

--- src/main/java/org/computate/frFR/java/EcrireApiClasse.java
+++ src/main/java/org/computate/frFR/java/EcrireApiClasse.java
@@ -15,6 +15,7 @@ package org.computate.frFR.java;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -2424,7 +2425,8 @@ public class EcrireApiClasse extends EcrireGenClasse {
 							tl(8, "Object bodyVal = body.getValue(f);");
 							tl(8, "if(bodyVal instanceof JsonArray) {");
 							tl(9, "JsonArray bodyVals = (JsonArray)bodyVal;");
-							tl(9, "Collection<?> vals = bodyVals.getList();");
+							tl(9, "Object valsObj = o.", classeLangueConfig.getString(ConfigCles.var_obtenir), classeLangueConfig.getString(ConfigCles.var_PourClasse), "(f);");
+							tl(9, "Collection<?> vals = valsObj instanceof JsonArray ? ((JsonArray)valsObj).getList() : (Collection<?>)valsObj;");
 							tl(9, "if(bodyVals.size() == vals.size()) {");
 							tl(10, "Boolean match = true;");
 							tl(10, "for(Object val : vals) {");
