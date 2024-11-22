17e5bd59dd18e5f1308a9b87e007a88bec556ad9
Christopher Tate <computate@computate.org>
2024-11-22 10:18:15 -0700

    Adding 404 to some requests

--- src/main/java/org/computate/frFR/java/EcrireApiClasse.java
+++ src/main/java/org/computate/frFR/java/EcrireApiClasse.java
@@ -3756,7 +3756,15 @@ public class EcrireApiClasse extends EcrireGenClasse {
 						tl(3, "});");
 					}
 					else {
-						tl(3, "promise.complete(ServiceResponse.completedWithJson(Buffer.buffer(Optional.ofNullable(json).orElse(new JsonObject()).encodePrettily())));");
+						tl(3, "if(json == null) {");
+						tl(4, "String ", classeVarId, " = ", i18nGlobale.getString(I18n.var_requeteSite), ".get", i18nGlobale.getString(I18n.var_RequeteService), "().getParams().getJsonObject(\"path\").getString(\"", classeVarId, "\");");
+						tl(6, "String m = String.format(\"", i18nGlobale.getString(I18n.str_s_s_non_trouve), "\", \"", classeNomAdjectifSingulier, "\", ", classeVarId, ");");
+						tl(4, "promise.complete(new ServiceResponse(404");
+						tl(6, ", m");
+						tl(6, ", Buffer.buffer(new JsonObject().put(\"", i18nGlobale.getString(I18n.var_message), "\", m).encodePrettily()), null));");
+						tl(3, "} else {");
+						tl(4, "promise.complete(ServiceResponse.completedWithJson(Buffer.buffer(Optional.ofNullable(json).orElse(new JsonObject()).encodePrettily())));");
+						tl(3, "}");
 					}
 	
 					tl(2, "} catch(Exception ex) {");
